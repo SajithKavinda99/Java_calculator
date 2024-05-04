d3068e52021812ac9d52d947779bb3262907b433
SajithKavinda99 <sajithbusiness094@gmail.com>
2024-05-05 00:06:30 +0530

    Initial commit

---
+++ src/java_calculator/Java_calculator.java
@@ -0,0 +1,11 @@
+
+package java_calculator;
+
+public class Java_calculator {
+    public static void main(String[] args) {
+        
+        calculator start = new calculator();
+        start.setVisible(true);
+    }
+    
+}
